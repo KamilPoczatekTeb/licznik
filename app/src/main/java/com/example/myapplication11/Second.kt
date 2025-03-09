c15e98346dcb7f5f3bacdaac890864a9b5c996fc
Kamil Początek <kamilpocz2@gmail.com>
2025-03-09 14:47:07 +0100

    Add intents

--- app/src/main/java/com/example/myapplication11/Second.kt
+++ app/src/main/java/com/example/myapplication11/Second.kt
@@ -1,38 +1,29 @@
 package com.example.myapplication11
 
-import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Second : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
 
-        var number = 0
-        val intent = Intent(this, Second::class.java)
+        val backButton = findViewById<Button>(R.id.back_button)
 
-        val textField = findViewById<TextView>(R.id.text_field)
-        val button1 = findViewById<Button>(R.id.button1)
-        button1.setOnClickListener{
-            startActivity(intent)
+        backButton.setOnClickListener{
+            finish()
         }
-
-
 
 
     }
